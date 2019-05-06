36640a85db7ab0e16a7a779eea409d09f062cf05
storm1g <storm94@hotmail.rs>
2019-05-06 16:49:39 +0200

    small changes and fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,169 +1,173 @@
-let tanks,
-    playerStats;
+//document  
+  let tanks,
+      playerStats;
 
 
-getTanks();
+  getTanks();
 
-// Gets a list of all tanks in the game with: is_premium, contour_icon, short_name, nation, tier
-function getTanks(){
-  $.ajax({
-    method: "GET",
-    url: "https://api.worldoftanks.eu/wot/encyclopedia/vehicles/?application_id=6d2ad8ec3cf857de529a60c5ce6f73f0&fields=is_premium%2C+images.contour_icon%2C+type%2C+short_name%2C+nation%2C+tier%2C",
-    dataType: "json"
-  }).done(function(data){
-    tanks = data.data;
-    console.log('List of tanks retrieved.');
-  });
-};
-
-
-// Search button event listener
-$('#search form button').on("click", displayStats);
-
-$('#moe').on("click", function(){
-  playerStats.sort(function (a, b) {
-    return b.all.marksOnGun - a.all.marksOnGun;
-  });
-  makeTable();
-});
-
-$('#mastery').on("click", function(){
-  playerStats.sort(function (a, b) {
-    return b.mark_of_mastery - a.mark_of_mastery;
-  });
-  makeTable();
-});
-
-$('#battles').on("click", function(){
-  playerStats.sort(function (a, b) {
-    return b.all.battles - a.all.battles;
-  });
-  makeTable();
-})
-
-// Gets players' stats and creates a complete table with all data
-function displayStats(){
-  let playerID;
-  let playerName = $('#search form input').val();
-  let server = $("#inlineFormCustomSelectPref").val();
-
-  $("table").addClass("hidden")
-  $(".loading-ring").removeClass("hidden");
-
-  $.when(
-    // Gets a players ID
+  // Gets a list of all tanks in the game with: is_premium, contour_icon, short_name, nation, tier
+  function getTanks(){
     $.ajax({
       method: "GET",
-      url: `https://api.worldoftanks.${server}/wot/account/list/?application_id=6d2ad8ec3cf857de529a60c5ce6f73f0&search=${playerName}&type=exact`,
+      url: "https://api.worldoftanks.eu/wot/encyclopedia/vehicles/?application_id=6d2ad8ec3cf857de529a60c5ce6f73f0&fields=is_premium%2C+images.contour_icon%2C+type%2C+short_name%2C+nation%2C+tier%2C",
       dataType: "json"
     }).done(function(data){
-      playerID = data.data[0].account_id;
-      console.log('Players\' ID retrieved.' + playerID);
-    })
-  ).then(function() {
+      tanks = data.data;
+      console.log('List of tanks retrieved.');
+    });
+  };
+
+
+  // Search button event listener
+  $('#search form button').on("click", displayStats);
+
+  // Sorting
+  $('#moe').on("click", function(){
+    playerStats.sort(sortBy("all", "marksOnGun"));
+    makeTable();
+  });
+
+  $('#mastery').on("click", function(){
+    playerStats.sort(sortBy("mark_of_mastery"));
+    makeTable();
+  });
+
+  $('#battles').on("click", function(){
+    playerStats.sort(sortBy("all", "battles"));
+    makeTable();
+  })
+
+  // Gets players' stats and creates a complete table with all data
+  function displayStats(){
+    // prevents page reloading bug after using search
+    event.preventDefault();
+
+    let playerID;
+    let playerName = $('#search form input').val();
+    let server = $("#inlineFormCustomSelectPref").val();
+
+    $("table").addClass("hidden")
+    $(".loading-ring").removeClass("hidden");
+
     $.when(
-  // Gets a list of players' tanks + wins, battles, damage dealt, avg. xp and mark of mastery (0-4)
-    $.ajax({
-      method: "GET",
-      url: `https://api.worldoftanks.${server}/wot/tanks/stats/?application_id=6d2ad8ec3cf857de529a60c5ce6f73f0&account_id=${playerID}&fields=all.damage_dealt%2C+all.battles%2C+all.battle_avg_xp%2C+all.wins%2C+mark_of_mastery%2C+tank_id`,
-      dataType: "json"
-    }).done(function(data){
-      playerStats = data.data[playerID];
-      console.log('Players\' stats retrieved.');
-    }),
-
-  // Gets a list of players' achievements including Marks of Excellence
-    ).then(function() {
+      // Gets player ID
       $.ajax({
         method: "GET",
-        url: `https://api.worldoftanks.${server}/wot/tanks/achievements/?application_id=6d2ad8ec3cf857de529a60c5ce6f73f0&account_id=${playerID}&fields=achievements%2C+tank_id`,
+        url: `https://api.worldoftanks.${server}/wot/account/list/?application_id=6d2ad8ec3cf857de529a60c5ce6f73f0&search=${playerName}&type=exact`,
         dataType: "json"
       }).done(function(data){
-        for (let i = 0; i < data.data[playerID].length; i++){
-          if (data.data[playerID][i].achievements.marksOnGun){
-            playerStats[i].all.marksOnGun = data.data[playerID][i].achievements.marksOnGun;
-          } else {
-            playerStats[i].all.marksOnGun = 0;
-          }
-        }
-        // playerMoe = data.data[playerID];
-        console.log('Players\' Marks of Excellence retrieved and stored into playerStats.');
-        playerStats.sort(function (a, b) {
-          return b.all.battles - a.all.battles;
-        });
-        console.log('playerStats sorted by number of battles');
-        makeTable();
+        playerID = data.data[0].account_id;
+        console.log('Players\' ID retrieved.' + playerID);
       })
+    ).then(function() {
+      $.when(
+    // Gets a list of players' tanks + wins, battles, damage dealt, avg. xp and mark of mastery (0-4)
+      $.ajax({
+        method: "GET",
+        url: `https://api.worldoftanks.${server}/wot/tanks/stats/?application_id=6d2ad8ec3cf857de529a60c5ce6f73f0&account_id=${playerID}&fields=all.damage_dealt%2C+all.battles%2C+all.battle_avg_xp%2C+all.wins%2C+mark_of_mastery%2C+tank_id`,
+        dataType: "json"
+      }).done(function(data){
+        playerStats = data.data[playerID];
+        console.log('Players\' stats retrieved.');
+      }),
+
+    // Gets a list of players' achievements including Marks of Excellence
+      ).then(function() {
+        $.ajax({
+          method: "GET",
+          url: `https://api.worldoftanks.${server}/wot/tanks/achievements/?application_id=6d2ad8ec3cf857de529a60c5ce6f73f0&account_id=${playerID}&fields=achievements%2C+tank_id`,
+          dataType: "json"
+        }).done(function(data){
+          for (let i = 0; i < data.data[playerID].length; i++){
+            if (data.data[playerID][i].achievements.marksOnGun){
+              playerStats[i].all.marksOnGun = data.data[playerID][i].achievements.marksOnGun;
+            } else {
+              playerStats[i].all.marksOnGun = 0;
+            }
+          }
+          console.log('Players\' Marks of Excellence retrieved and stored into playerStats.');
+          
+          playerStats.sort(sortBy("all", "battles"));
+          console.log('playerStats sorted by number of battles');
+          makeTable();
+          console.log('done!');
+        })
+      });
     });
-  });
-};
+  };
 
-function makeTable(){
-  console.log("making a table");
-  
-  $('table tbody').html("");
+  function makeTable(){
+    console.log("making a table");
+    
+    $('table tbody').html("");
 
-  for (let i = 0, output; i < playerStats.length; i++){
+    for (let i = 0, output; i < playerStats.length; i++){
 
-    if (tanks[playerStats[i].tank_id] === undefined) {
-      continue
+      // Checks if there are any stats for the tank (wot API ignores old, removed/replaced, and some premium tanks)
+      if (tanks[playerStats[i].tank_id] === undefined) {
+        console.log(i);
+        continue
+      }
+
+      output = '<tr>' + 
+          '<th><img src=' + tanks[playerStats[i].tank_id].images.contour_icon + '></th>' + 
+          '<td>' + tanks[playerStats[i].tank_id].short_name + '</td>' + 
+          '<td>' + '<img src="assets/img/flags/' + tanks[playerStats[i].tank_id].nation +'.png"></td>' +
+          '<td>' + '<span class="tanktype ' + tanks[playerStats[i].tank_id].type + '"></span></td>' +
+          '<td>' + tanks[playerStats[i].tank_id].tier + '</td>' +
+          '<td>' + Number(Math.round(playerStats[i].all.damage_dealt / playerStats[i].all.battles +"e2")+"e-2") + '</td>' +
+          '<td>' + playerStats[i].all.battle_avg_xp + '</td>' +
+          '<td>' + playerStats[i].all.battles + '</td>' +
+          '<td>' + Number(Math.round(playerStats[i].all.wins / playerStats[i].all.battles +"e4") + "e-2") + "%" + '</td>';
+
+          if (playerStats[i].mark_of_mastery){
+            output += '<td>' + '<img src="assets/img/mastery/' + playerStats[i].mark_of_mastery + '.png"></td>';
+          } else {
+            output += '<td></td>'
+          };
+
+          if (playerStats[i].all.marksOnGun){
+            output += '<td>' + '<img src="assets/img/marks/' + tanks[playerStats[i].tank_id].nation + '_' + playerStats[i].all.marksOnGun + '.png" class="moe"></td>';
+          } else {
+            output += '<td></td>';
+          };
+
+      output += '</tr>';
+
+      $(output).appendTo("table tbody");
     }
 
-    output = '<tr>' + 
-        '<th><img src=' + tanks[playerStats[i].tank_id].images.contour_icon + '></th>' + 
-        '<td>' + tanks[playerStats[i].tank_id].short_name + '</td>' + 
-        '<td>' + '<img src="assets/img/flags/' + tanks[playerStats[i].tank_id].nation +'.png"></td>' +
-        '<td>' + '<span class="tanktype ' + tanks[playerStats[i].tank_id].type + '"></span></td>' +
-        '<td>' + tanks[playerStats[i].tank_id].tier + '</td>' +
-        '<td>' + Number(Math.round(playerStats[i].all.damage_dealt / playerStats[i].all.battles +"e2")+"e-2") + '</td>' +
-        '<td>' + playerStats[i].all.battle_avg_xp + '</td>' +
-        '<td>' + playerStats[i].all.battles + '</td>' +
-        '<td>' + Number(Math.round(playerStats[i].all.wins / playerStats[i].all.battles +"e4") + "e-2") + "%" + '</td>';
+    $(".loading-ring").addClass("hidden");
+    $("table").removeClass("hidden");
+  };
 
-        if (playerStats[i].mark_of_mastery){
-          output += '<td>' + '<img src="assets/img/mastery/' + playerStats[i].mark_of_mastery + '.png"></td>';
-        } else {
-          output += '<td></td>'
-        };
-
-        if (playerStats[i].all.marksOnGun){
-          output += '<td>' + '<img src="assets/img/marks/' + tanks[playerStats[i].tank_id].nation + '_' + playerStats[i].all.marksOnGun + '.png" class="moe"></td>';
-        } else {
-          output += '<td></td>';
-        };
-
-    output += '</tr>';
-
-    $(output).appendTo("table tbody");
+  function sortBy(prop1, prop2) {
+    return function(a, b) {
+      if (prop2) {
+        return b[prop1][prop2] - a[prop1][prop2];
+      } else {
+          return b[prop1] - a[prop1];
+      }
+    }
   }
 
-  $(".loading-ring").addClass("hidden");
-  $("table").removeClass("hidden");
-};
 
-function propComparator(prop) {
-  return function(a, b) {
-      return b[prop] - a[prop];
-  }
-}
+
+  // playerStats.sort(sortBy('batt'));
+
+  // playerStats[0].all.markOfMastery = playerMoe[0].achievements.markOfMastery - dodavanje posebnih statova
 
 
 
-// playerStats.sort(propComparator('batt'));
-
-// playerStats[0].all.markOfMastery = playerMoe[0].achievements.markOfMastery - dodavanje posebnih statova
-
-
-
-/* $.ajax({
-        method: "GET",
-        url: "https://api.worldoftanks.eu/wot/tanks/achievements/?application_id=6d2ad8ec3cf857de529a60c5ce6f73f0&account_id=" + playerID + "&fields=achievements%2C+tank_id",
-        dataType: "json"
-      }).done(function(data){
-        for (let i = 0; i < data.data[playerID].length; i++){
-          playerStats[i].all.marksOnGun = data.data[playerID][i].achievements.marksOnGun;
-        }
-        // playerMoe = data.data[playerID];
-        console.log('Players\' achievements retrieved.');
-      })
-*/
+  /* $.ajax({
+          method: "GET",
+          url: "https://api.worldoftanks.eu/wot/tanks/achievements/?application_id=6d2ad8ec3cf857de529a60c5ce6f73f0&account_id=" + playerID + "&fields=achievements%2C+tank_id",
+          dataType: "json"
+        }).done(function(data){
+          for (let i = 0; i < data.data[playerID].length; i++){
+            playerStats[i].all.marksOnGun = data.data[playerID][i].achievements.marksOnGun;
+          }
+          // playerMoe = data.data[playerID];
+          console.log('Players\' achievements retrieved.');
+        })
+  */
